d631b5c5d856813fefbab8b540c35da7d30c316a
CryoFlar3 <ndukes@computermentors.org>
2018-09-18 19:05:53 -0400

    Initial commit

---
+++ app/src/main/java/org/computermentors/interactivestory/MainActivity.java
@@ -0,0 +1,38 @@
+package org.computermentors.interactivestory;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText nameField;
+    private Button submitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameField = findViewById(R.id.nameEditText);
+        submitButton = findViewById(R.id.startButton);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameField.getText().toString();
+                //Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+                startStory(name);
+            }
+        });
+
+    }
+    private void startStory(String name){
+        Intent intent = new Intent(MainActivity.this, StoryActivity.class);
+        intent.putExtra("name", name);
+        startActivity(intent);
+    }
+}
